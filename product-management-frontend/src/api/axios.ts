4633e701c2e7368f463cd53152c512281103101b
Ayush Sandilya <97401472+ayushsandilya@users.noreply.github.com>
2025-05-15 10:57:59 +0530

    Update axios.ts

--- product-management-frontend/src/api/axios.ts
+++ product-management-frontend/src/api/axios.ts
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
 
-const API = axios.create({
-  baseURL: baseURL,
+export default axios.create({
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
-export default API;

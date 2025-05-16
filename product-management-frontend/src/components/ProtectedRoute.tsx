7ba52e82abd4ae620245518f2d0b8d75bd905c5a
Ayush Sandilya <97401472+ayushsandilya@users.noreply.github.com>
2025-05-16 11:14:59 +0530

    Delete product-management-frontend directory

--- product-management-frontend/src/components/ProtectedRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { ReactElement } from 'react';
-import { JSX } from 'react/jsx-runtime';
-
-export default function ProtectedRoute({ children }: { children: ReactElement }): JSX.Element {
-  const token = localStorage.getItem('token');
-  return token ? children : <Navigate to="/login" replace />;
-}

fed1eff7212e22806acdb7be781bca0114eaf50a
Ayush Sandilya <97401472+ayushsandilya@users.noreply.github.com>
2025-05-16 22:36:09 +0530

    Update axios.ts

--- product-management-frontend/src/api/axios.ts
+++ product-management-frontend/src/api/axios.ts
@@ -1,10 +1,11 @@
+// src/api/axios.ts
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://product-management-backend-hr6h.onrender.com';
-
-export default axios.create({
-  baseURL: API_BASE_URL,
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
+export default instance;
